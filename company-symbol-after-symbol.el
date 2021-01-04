f8d73685e89ed0a572c4e560bcf1da9da1f90da8
zk-phi <zk-phi@users.noreply.github.com>
2021-01-04 13:28:46 +0900

    Implement thresholding

--- company-symbol-after-symbol.el
+++ company-symbol-after-symbol.el
@@ -1,4 +1,3 @@
-;; TODO: thresholding
 ;; TODO: cache candidates across sessions
 
 ;; (defvar-local company-symbol-after-symbol-table nil)
@@ -35,6 +34,12 @@ least one non-space character is required to start completon."
   :group 'company-symbol-after-symbol
   :type 'boolean)
 
+(defcustom company-symbol-after-symbol-same-buffer-occurrence-threshold 2
+  "Number of minimum ocurrences when filtering candidates in the
+current buffer."
+  :group 'company-symbol-after-symbol
+  :type 'integer)
+
 (defun company-symbol-after-symbol-search-candidates (prefix &optional cursor)
   (let ((regex (concat (regexp-quote prefix) "[\s\t]*\\(\\_<.+?\\_>\\)"))
         lst)
@@ -48,6 +53,20 @@ least one non-space character is required to start completon."
       (while (search-forward-regexp regex nil t)
         (push (match-string-no-properties 0) lst)))
     lst))
+
+(defun company-symbol-after-symbol-filter-by-ocurrences (sorted-list threshold)
+  (when sorted-list
+    (let ((current-count 1) candidates)
+      (while sorted-list
+        (cond ((and (cadr sorted-list) (string= (car sorted-list) (cadr sorted-list)))
+               (pop sorted-list)
+               (cl-incf current-count))
+              (t
+               (if (>= current-count threshold)
+                   (push (pop sorted-list) candidates)
+                 (pop sorted-list))
+               (setq current-count 1))))
+      candidates)))
 
 (defvar company-symbol-after-symbol--candidates nil)
 
@@ -68,8 +87,11 @@ least one non-space character is required to start completon."
     (duplicates t)
     (candidates
      (or company-symbol-after-symbol--candidates
-         (setq company-symbol-after-symbol--candidates
-               (company-symbol-after-symbol-search-candidates company-prefix (point)))))))
+         (let ((candidates (company-symbol-after-symbol-search-candidates company-prefix (point))))
+          (setq company-symbol-after-symbol--candidates
+                (company-symbol-after-symbol-filter-by-ocurrences
+                 (sort candidates 'string<)
+                 company-symbol-after-symbol-same-buffer-occurrence-threshold)))))))
 
 (defun company-symbol-after-symbol-finished (&optional _)
   (setq company-symbol-after-symbol--candidates nil))
