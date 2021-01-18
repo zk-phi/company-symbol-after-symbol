29f74ecc1350665fe461cbef76bae7901a55ca1b
zk-phi <zk-phi@users.noreply.github.com>
2021-01-18 17:31:14 +0900

    Complete symbols from other buffers

--- company-symbol-after-symbol.el
+++ company-symbol-after-symbol.el
@@ -64,10 +64,123 @@ is specified, search before/after the point separately."
                (setq current-count 1))))
       candidates)))
 
+;; ---- completion-tree
+
+(defun company-symbol-after-symbol-candidates-tree-empty ()
+  "Allocate an empty candidates-tree."
+  ;; cons[ocurrences, tree[symb, cons[ocurrences, nil]]]
+  (cons 0 nil))
+
+(defun company-symbol-after-symbol-candidates-tree-insert (tree symbol)
+  "Insert item to a candidates-tree destructively."
+  (let ((cell (radix-tree-lookup (cdr tree) symbol)))
+    (if cell
+        (cl-incf (car cell))
+      (setcdr tree (radix-tree-insert (cdr tree) symbol (cons 1 nil)))))
+  (cl-incf (car tree)))
+
+(defun company-symbol-after-symbol-completion-tree-empty ()
+  "Allocate an empty completion-tree."
+  ;; cons[tree[prefix, candidates-tree], nil]
+  (cons nil nil))
+
+(defun company-symbol-after-symbol-completion-tree-insert (tree prefix symbol)
+  "Insert item to a completion-tree destructively."
+  (let ((candidates-tree (radix-tree-lookup (car tree) prefix)))
+    (unless candidates-tree
+      (setq candidates-tree (company-symbol-after-symbol-candidates-tree-empty))
+      (setcar tree (radix-tree-insert (car tree) prefix candidates-tree)))
+    (company-symbol-after-symbol-candidates-tree-insert candidates-tree symbol)))
+
+(defun company-symbol-after-symbol-completion-tree-search (tree prefix &optional threshold)
+  "Search through a completion-tree with PREFIX. If THRESHOLD is
+specified, filter as like
+`company-symbol-after-symbol-filter-by-occurrences'."
+  (let ((tree (or (radix-tree-lookup (car tree) prefix) (cons 0 nil)))
+        candidates)
+    (setq threshold (* (or threshold 0) (car tree)))
+    (radix-tree-iter-mappings
+     (cdr tree)
+     (lambda (k v) (when (>= (car v) threshold) (push (concat prefix k) candidates))))
+    candidates))
+
+;; ---- cache
+
+;; hash[mode -> completion-tree]
+(defvar company-symbol-after-symbol-cache (make-hash-table :test 'eq))
+(defvar-local company-symbol-after-symbol-cache-is-dirty t)
+
+(defun company-symbol-after-symbol-get-all-current-buffer-candidates ()
+  "Get list of all possible (PREFIX . COMPLETION) pairs in the
+buffer."
+  (let ((lines (mapcar
+                (lambda (line) (cdr (split-string line "\\_<\\|\\_>")))
+                (split-string (buffer-substring-no-properties (point-min) (point-max)) "\n")))
+        candidates)
+    (dolist (line lines)
+      ;; first symbol (2-gram)
+      (when (cddr line)
+        (push (cons (concat (car line) (cadr line)) (car (cddr line))) candidates))
+      ;; rest (3-gram)
+      (while (cddddr line)
+        (push (cons (concat (car line) (cadr line) (caddr line) (cadddr line))
+                    (car (cddddr line)))
+              candidates)
+        (setq line (cddr line))))
+    candidates))
+
+(defun company-symbol-after-symbol-update-cache (&optional buffer)
+  "Put all symbols in the buffer into
+`company-symbol-after-symbol-cache'."
+  (with-current-buffer (or buffer (current-buffer))
+    (when (and company-symbol-after-symbol-cache-is-dirty
+               (or (derived-mode-p 'prog-mode)
+                   (derived-mode-p 'text-mode)))
+      (let ((tree (gethash major-mode company-symbol-after-symbol-cache nil))
+            (pairs (company-symbol-after-symbol-get-all-current-buffer-candidates)))
+        (unless tree
+          (setq tree (company-symbol-after-symbol-completion-tree-empty))
+          (puthash major-mode tree company-symbol-after-symbol-cache))
+        (dolist (pair pairs)
+          (company-symbol-after-symbol-completion-tree-insert tree (car pair) (cdr pair)))
+        (setq company-symbol-after-symbol-cache-is-dirty nil)))))
+
+(defun company-symbol-after-symbol-invalidate-cache (&rest _)
+  (setq company-symbol-after-symbol-cache-is-dirty t))
+
+(defun company-symbol-after-symbol-update-cache-other-buffers ()
+  "Update cache for all buffers except for the current buffer."
+  (dolist (buf (buffer-list))
+    (unless (eq buf (current-buffer))
+      (company-symbol-after-symbol-update-cache buf))))
+
 ;; ---- interface
 
 (defvar company-symbol-after-symbol--candidates nil)
 (defvar company-symbol-after-symbol--bolp nil)
+
+(defun company-symbol-after-symbol-search-current-buffer-candidates (prefix)
+  (let ((candidates
+         (company-symbol-after-symbol-search-regex
+          (concat (and company-symbol-after-symbol--bolp "^\\W*")
+                  "\\(" (regexp-quote prefix) "\\_<.+?\\_>\\)")
+          1
+          (point))))
+    (setq company-symbol-after-symbol--candidates
+          (company-symbol-after-symbol-filter-by-occurrences
+           (sort candidates 'string<)
+           company-symbol-after-symbol-same-buffer-threshold))))
+
+(defun company-symbol-after-symbol-search-other-buffer-candidates (prefix)
+  (company-symbol-after-symbol-update-cache-other-buffers)
+  (company-symbol-after-symbol-completion-tree-search
+   (gethash major-mode company-symbol-after-symbol-cache nil)
+   prefix
+   company-symbol-after-symbol-same-buffer-threshold))
+
+(defun company-symbol-after-symbol-all-completions (prefix)
+  (nconc (company-symbol-after-symbol-search-current-buffer-candidates prefix)
+         (company-symbol-after-symbol-search-other-buffer-candidates prefix)))
 
 (defun company-symbol-after-symbol (command &optional arg &rest ignored)
   (interactive (list 'interactive))
@@ -97,16 +210,7 @@ is specified, search before/after the point separately."
     (duplicates t)
     (candidates
      (or company-symbol-after-symbol--candidates
-         (let ((candidates
-                (company-symbol-after-symbol-search-regex
-                 (concat (and company-symbol-after-symbol--bolp "^\\W*")
-                         "\\(" (regexp-quote company-prefix) "\\_<.+?\\_>\\)")
-                 1
-                 (point))))
-           (setq company-symbol-after-symbol--candidates
-                 (company-symbol-after-symbol-filter-by-occurrences
-                  (sort candidates 'string<)
-                  company-symbol-after-symbol-same-buffer-threshold)))))))
+         (company-symbol-after-symbol-all-completions company-prefix)))))
 
 (defun company-symbol-after-symbol-finished (&optional _)
   (setq company-symbol-after-symbol--candidates nil))
